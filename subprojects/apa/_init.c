67eb2d0070899e1d4525954247a4362380d1df63
uyjulian <uyjulian@gmail.com>
2023-05-29 14:59:14 -0500

    Allow pass param to apa

--- subprojects/apa/_init.c
+++ subprojects/apa/_init.c
@@ -8,6 +8,12 @@ int _init_apa(int argc, char *argv[])
         {
             "ps2hdd.irx",
             NULL};
-    int result = apa_start(1, args);
+    int pass_argc = 1;
+    char **pass_argv = args;
+    if (argv != NULL) {
+        pass_argc = argc;
+        pass_argv = argv;
+    }
+    int result = apa_start(pass_argc, pass_argv);
     return (result);
 }
