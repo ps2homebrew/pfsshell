4fbbd23396f6a1f9cfe770e54d2cd337bb15f747
Julian Uy <uyjulian@gmail.com>
2025-04-06 05:50:59 -0500

    feat: support passing fd to fake atad

--- subprojects/fakeps2sdk/atad.c
+++ subprojects/fakeps2sdk/atad.c
@@ -22,6 +22,10 @@
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+#ifndef O_BINARY
+#define O_BINARY 0
+#endif
+
 /* These are used with the dir parameter of ata_device_sector_io().  */
 #define ATA_DIR_READ  0
 #define ATA_DIR_WRITE 1
@@ -37,15 +41,70 @@ typedef struct _ata_devinfo
 static int handle = -1;
 
 static u32 hdd_length = 0; /* in sectors */
-static const char *atad_device_path = NULL;
+
+int set_atad_device_handle(int fd)
+{
+#ifdef _WIN32
+    HANDLE win_handle = CreateFileA(atad_device_path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+#endif
+#ifdef __APPLE__
+    u64 size = 0, sector_count = 0;
+    u32 sector_size = 0;
+    if (ioctl(fd, DKIOCGETBLOCKCOUNT, &sector_count) == 0) {
+        ioctl(fd, DKIOCGETBLOCKSIZE, &sector_size);
+        if (sector_size != 512)
+            size = ((sector_count * sector_size) - 511) / 512;
+        else
+            size = sector_count;
+        if ((int64_t)size >= 0)
+            hdd_length = size;
+        else
+            return 1;
+    } else if (errno == ENOTTY) {
+        /* Not a device. Fall back to lseek */
+#endif
+#ifdef _WIN32
+        DISK_GEOMETRY geo;
+        DWORD len = 0;
+        u64 size = 0, size_in_bytes = 0;
+        if (win_handle != INVALID_HANDLE_VALUE && DeviceIoControl(win_handle, IOCTL_DISK_GET_DRIVE_GEOMETRY, NULL, 0, &geo, sizeof(DISK_GEOMETRY), &len, NULL)) {
+            size_in_bytes = (geo.Cylinders.QuadPart * geo.TracksPerCylinder * geo.SectorsPerTrack * geo.BytesPerSector);
+            size = ((size_in_bytes)-511) / 512;
+            if ((int64_t)size >= 0) {
+                hdd_length = size;
+                CloseHandle(win_handle);
+                win_handle = INVALID_HANDLE_VALUE;
+            } else
+                return 1;
+        } else {
+#endif
+            off_t size = lseek(fd, 0, SEEK_END);
+            if (size != (off_t)-1)
+                hdd_length = (size - 511) / 512;
+            else
+                return 1;
+#ifdef _WIN32
+        }
+#endif
+#ifdef __APPLE__
+    }
+#endif
+#ifdef _WIN32
+    if (win_handle != INVALID_HANDLE_VALUE) {
+        CloseHandle(win_handle);
+    }
+#endif
+    handle = fd;
+    return 0;
+}
 
 void set_atad_device_path(const char *path)
 {
-    if (atad_device_path != NULL) {
-        free((char *)atad_device_path);
-        atad_device_path = NULL;
+    int fd;
+    fd = open(path, O_RDWR | O_BINARY);
+    if (fd == -1 || set_atad_device_handle(fd)) {
+        perror(path), exit(1);
     }
-    atad_device_path = strdup(path);
 }
 
 void atad_close(void)
@@ -54,78 +113,10 @@ void atad_close(void)
         close(handle), handle = -1;
 }
 
-void init(void)
-{
-    if (atad_device_path == NULL) {
-        perror("File path not set"), exit(1);
-    }
-#ifdef _WIN32
-    HANDLE win_handle = CreateFileA(atad_device_path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-#endif
-    handle = open(atad_device_path, O_RDWR |
-#ifdef O_BINARY
-                                        O_BINARY
-#else
-                                        0
-#endif
-    );
-    if (handle != -1) {
-#ifdef __APPLE__
-        u64 size = 0, sector_count = 0;
-        u32 sector_size = 0;
-        if (ioctl(handle, DKIOCGETBLOCKCOUNT, &sector_count) == 0) {
-            ioctl(handle, DKIOCGETBLOCKSIZE, &sector_size);
-            if (sector_size != 512)
-                size = ((sector_count * sector_size) - 511) / 512;
-            else
-                size = sector_count;
-            if ((int64_t)size >= 0)
-                hdd_length = size;
-            else
-                perror(atad_device_path), exit(1);
-        } else if (errno == ENOTTY) {
-            /* Not a device. Fall back to lseek */
-#endif
-#ifdef _WIN32
-            DISK_GEOMETRY geo;
-            DWORD len = 0;
-            u64 size = 0, size_in_bytes = 0;
-            if (win_handle != INVALID_HANDLE_VALUE && DeviceIoControl(win_handle, IOCTL_DISK_GET_DRIVE_GEOMETRY, NULL, 0, &geo, sizeof(DISK_GEOMETRY), &len, NULL)) {
-                size_in_bytes = (geo.Cylinders.QuadPart * geo.TracksPerCylinder * geo.SectorsPerTrack * geo.BytesPerSector);
-                size = ((size_in_bytes)-511) / 512;
-                if ((int64_t)size >= 0)
-                    hdd_length = size;
-                else
-                    perror(atad_device_path), exit(1);
-            } else {
-#endif
-                off_t size = lseek(handle, 0, SEEK_END);
-                if (size != (off_t)-1)
-                    hdd_length = (size - 511) / 512;
-                else
-                    perror(atad_device_path), exit(1);
-#ifdef _WIN32
-            }
-#endif
-#ifdef __APPLE__
-        }
-#endif
-    } else
-        perror(atad_device_path), exit(1);
-#ifdef _WIN32
-    if (win_handle != INVALID_HANDLE_VALUE) {
-        CloseHandle(win_handle);
-    }
-#endif
-}
-
 ata_devinfo_t *ata_get_devinfo(int device)
 {
-    if (handle == -1)
-        init();
-
     static ata_devinfo_t info;
-    if (device == 0) {
+    if (device == 0 && handle != -1) {
         info.exists = 1;
         info.has_packet = 0;
         info.total_sectors = hdd_length;
@@ -141,8 +132,10 @@ ata_devinfo_t *ata_get_devinfo(int device)
 
 int ata_device_sector_io(int device, void *buf, u32 lba, u32 nsectors, int dir)
 {
-    if (handle == -1)
-        init();
+    if (handle == -1) {
+        printf("atadDmaTransfer: device fd not set\n");
+        return (-1);
+    }
 
     if (device != 0) {
         printf("atadDmaTransfer: invalid device %d\n", device);
@@ -151,7 +144,7 @@ int ata_device_sector_io(int device, void *buf, u32 lba, u32 nsectors, int dir)
 
     off_t pos = lseek(handle, (off_t)lba * 512, SEEK_SET);
     if (pos == (off_t)-1) {
-        printf("lseek: %s: %s\n", atad_device_path, strerror(errno));
+        printf("lseek: atad device fd %d: %s\n", handle, strerror(errno));
         return (-1);
     }
 
@@ -163,7 +156,7 @@ int ata_device_sector_io(int device, void *buf, u32 lba, u32 nsectors, int dir)
     if (len == nsectors * 512)
         return (0); /* success */
     else {
-        printf("read/write: %s: %s\n", atad_device_path, strerror(errno));
+        printf("read/write: atad device fd %d: %s\n", handle, strerror(errno));
         return (-1);
     }
 }
