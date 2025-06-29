d47d0712f97c4e9e31bd5b43784c3b48eb1c04dc
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2025-06-29 13:46:54 +0300

    pfs2tar: added --extract/--restore dummy flag Returned ability to control output tar filename

--- src/pfs2tar.c
+++ src/pfs2tar.c
@@ -316,7 +316,7 @@ static int tar_part(const char *arg)
 
 static void show_help(const char *progname)
 {
-    printf("usage: %s <device_path> [<optional_partition_name>]\n", progname);
+    printf("usage: %s --extract/--restore <ps2_hdd_device_path> [--partition <optional_partition_name>] [<tar_file>]\n", progname);
 }
 
 /* where (image of) PS2 HDD is; in fake_sdk/atad.c */
@@ -327,14 +327,52 @@ extern void atad_close(void); /* fake_sdk/atad.c */
 int main(int argc, char *argv[])
 {
     int result;
+    bool extract_mode = false;
 
     if (argc < 2) {
         show_help(argv[0]);
         return 1;
     }
 
-    set_atad_device_path(argv[1]);
+    if (strcmp(argv[1], "--extract") == 0)
+        extract_mode = true;
+    else if (strcmp(argv[1], "--restore") == 0)
+        extract_mode = false;
+    else {
+        show_help(argv[0]);
+        return 1;
+    }
 
+    // Get the input HDD image path
+    const char *hdd_path = argv[2];
+    const char *last_slash = strrchr(hdd_path, '/');
+    const char *filename = last_slash ? last_slash + 1 : hdd_path;
+
+    // Find the last dot for extension
+    const char *last_dot = strrchr(filename, '.');
+    size_t base_len = last_dot ? (size_t)(last_dot - filename) : strlen(filename);
+    char tar_filename[1024];
+    const char *partition_name = NULL;
+    if (argc >= 4) {
+        if (strncmp(argv[3], "--partition", 11) == 0) {
+            if (argc < 5) {
+                fprintf(stderr, "(!) Missing partition name.\n");
+                show_help(argv[0]);
+                return 1;
+            }
+            partition_name = argv[4];
+            if (argc > 5)
+                snprintf(tar_filename, sizeof(tar_filename), "%s", argv[5]);
+            else
+                snprintf(tar_filename, sizeof(tar_filename), "%s_%.*s.tar", partition_name, (int)base_len, filename);
+        } else {
+            snprintf(tar_filename, sizeof(tar_filename), "%s", argv[3]);
+        }
+    } else
+        snprintf(tar_filename, sizeof(tar_filename), "%.*s.tar", (int)base_len, filename);
+    printf("\n\nCreating tar file: %s\n", tar_filename);
+
+    set_atad_device_path(argv[2]);
     static const char *apa_args[] =
         {
             "ps2hdd.irx",
@@ -365,24 +403,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Get the input HDD image path
-    const char *input_path = argv[1];
-    const char *last_slash = strrchr(input_path, '/');
-    const char *filename = last_slash ? last_slash + 1 : input_path;
 
-    // Find the last dot for extension
-    const char *last_dot = strrchr(filename, '.');
-    size_t base_len = last_dot ? (size_t)(last_dot - filename) : strlen(filename);
-    const char *arg = (argc > 2) ? argv[2] : NULL;
-
-    char tar_filename[1024];
-    if (arg)
-        snprintf(tar_filename, sizeof(tar_filename), "%s_%.*s.tar", arg, (int)base_len, filename);
-    else
-        snprintf(tar_filename, sizeof(tar_filename), "%.*s.tar", (int)base_len, filename);
-    printf("\n\nCreating tar file: %s\n", tar_filename);
-
-    // Check if file exists
+    // Check if tar file already exists
     FILE *test_file = fopen(tar_filename, "rb");
     if (test_file != NULL) {
         fclose(test_file);
@@ -395,7 +417,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "(!) %s: %s.\n", tar_filename, strerror(errno));
         return 1;
     }
-    tar_part(arg);
+    tar_part(partition_name);
 
     fclose(tarfile_handle);
 
