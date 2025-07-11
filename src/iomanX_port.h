2d8b89aef3e60f8ba38d98bb9c7fab2b58b787f2
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2025-07-11 13:01:13 +0300

    Revert "Update ps2sdk"
    This reverts commit 7c372f81e569e4ad1dc4aeedd6f4feaf0acb6ffe.

--- src/iomanX_port.h
+++ src/iomanX_port.h
@@ -162,8 +162,8 @@ typedef struct
     /*1c*/ unsigned char mtime[8];
     /*24*/ unsigned int hisize;
     /*28*/ unsigned int private_0; // Number of subs (main) / subpart number (sub)
-    /*2c*/ unsigned int private_1; // Total size for main and sub partitions (main) / size of subpart (sub)
-    /*30*/ unsigned int private_2; // High size for private_1
+    /*2c*/ unsigned int private_1;
+    /*30*/ unsigned int private_2;
     /*34*/ unsigned int private_3;
     /*38*/ unsigned int private_4;
     /*3c*/ unsigned int private_5; /* Sector start.  */
